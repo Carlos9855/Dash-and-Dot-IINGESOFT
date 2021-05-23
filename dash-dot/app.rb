00c1d14fc3d76a655202deae4aeca8818002a1b2
carlos camacho <josecarloscamacho55@gmail.com>
2021-05-23 16:35:38 -0400

    Se agrego la sentencia para definir obstaculos

--- dash-dot/app.rb
+++ dash-dot/app.rb
@@ -12,23 +12,28 @@ end
 
 get '/inicio' do
     @entrada=params[:campo_entrada]
+    #Tablero
     @largo_tablero=@entrada.to_s.split(/\r\n/)[0].to_s.split(/,/)[0].to_i
     @alto_tablero=@entrada.to_s.split(/\r\n/)[0].to_s.split(/,/)[1].to_i    
-    
     tablero=Tablero.new(@largo_tablero,@alto_tablero)
     @largo_tablero1=tablero.getLargo
     @alto_tablero1=tablero.getAlto
-    
-    @pos_x_auto=@entrada.to_s.split(/\r\n/)[1].to_s.split(/,/)[0].to_i
-    @pos_y_auto=@entrada.to_s.split(/\r\n/)[1].to_s.split(/,/)[1].to_i
-    @orientacion_auto=@entrada.to_s.split(/\r\n/)[1].to_s.split("\s")[1]
+    #Definir obstaculo
+    @orien=@entrada.to_s.split(/\r\n/)[1].to_s.split(/\s/)[0]
+    @opsx=@entrada.to_s.split(/\r\n/)[1].to_s.split("\s")[1].to_i
+    @opsy=@entrada.to_s.split(/\r\n/)[1].to_s.split(/,/)[1].to_i
+    #Posicion Inicial del auto
+    @pos_x_auto=@entrada.to_s.split(/\r\n/)[2].to_s.split(/,/)[0].to_i
+    @pos_y_auto=@entrada.to_s.split(/\r\n/)[2].to_s.split(/,/)[1].to_i
+    @orientacion_auto=@entrada.to_s.split(/\r\n/)[2].to_s.split("\s")[1]
+    #Definir recorrido
     auto=Auto.new(@orientacion_auto,@pos_x_auto,@pos_y_auto)
     @pos_x_auto1=auto.getPosicion_x
     @pos_y_auto1=auto.getPosicion_y
     @orientacion_auto1=auto.getOrientacion
     tablero.addAuto(auto)
-    @comandos1=@entrada.to_s.split(/\r\n/)[2]
-    @comandos=@entrada.to_s.split(/\r\n/)[2].to_s.split(//)
+    @comandos1=@entrada.to_s.split(/\r\n/)[3]
+    @comandos=@entrada.to_s.split(/\r\n/)[3].to_s.split(//)
     @comandos.each do |c|
         if(c=="A")
             auto.avanzar(tablero.getLargo,tablero.getAlto)
